14f4655ad4cf1f1f741213ddfb773eae20af32d8
Wim Brand <fornax@fornaxian.com>
2019-05-07 23:16:29 +0200

    Init

---
+++ main.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"time"
+
+	sia "gitlab.com/NebulousLabs/Sia/node/api/client"
+)
+
+func main() {
+
+	sc := sia.New("127.0.0.1:9980")
+	sc.Password = "d0290de6ff4731a4de8da2f68cb7c3fe"
+
+	version, err := sc.DaemonVersionGet()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Connected to Sia %s (rev %s)\n", version.Version, version.GitRevision)
+
+	// Open the metrics CSV
+	created := false
+	f, err := os.OpenFile("metrics.csv", os.O_WRONLY, os.ModeAppend)
+	if os.IsNotExist(err) {
+		f, err = os.Create("metrics.csv")
+		if err != nil {
+			panic(err)
+		}
+		created = true
+	} else if err != nil {
+		panic(err)
+	}
+
+	// Append the data to the end of the file
+	f.Seek(0, os.SEEK_END)
+
+	csvWriter := csv.NewWriter(f)
+
+	if created {
+		// New file, print headers
+		err = csvWriter.Write(MetricsHeaders())
+		if err != nil {
+			panic(err)
+		}
+		csvWriter.Flush()
+	}
+
+	// In this loop we collect stats on the
+	//  - Files
+	//  - Contracts
+	//  - Wallet
+	//  - Allowance
+	//
+	// We store all this in a Metrics struct. When the information is complete
+	// we write the metrics to the CSV
+	var lastSize uint64
+	for {
+		// Sleep until the next full minute
+		time.Sleep(time.Until(time.Now().Add(time.Minute).Truncate(time.Minute)))
+
+		metrics, err := collectMetrics(sc)
+		if err != nil {
+			fmt.Printf("Error while collecting metrics: %s\n", err)
+			continue
+		}
+
+		err = metrics.WriteCSV(csvWriter)
+		if err != nil {
+			fmt.Printf("Error while writing to CSV: %s\n", err)
+		}
+		if err = csvWriter.Error(); err != nil {
+			fmt.Printf("Error while flushing CSV: %s\n", err)
+		}
+
+		fmt.Printf("[%s] Latency: %s Files: %d, Uploading: %d, Speed: %s/s\n",
+			metrics.Timestamp.Format("2006-01-02 15:04:05 -0700 MST"),
+			metrics.APILatency,
+			metrics.FileCount,
+			metrics.FileUploadsInProgressCount,
+			formatData((metrics.FileUploadedBytes-lastSize)/60),
+		)
+		lastSize = metrics.FileUploadedBytes
+
+		fmt.Println(metrics)
+	}
+}
+
+func collectMetrics(sc *sia.Client) (metrics Metrics, err error) {
+	metrics.Timestamp = time.Now()
+
+	// Collect file stats
+	files, err := sc.RenterFilesGet(true)
+	if err != nil {
+		return metrics, err
+	}
+	for _, file := range files.Files {
+		metrics.FileCount++
+		metrics.FileTotalBytes += file.Filesize
+		metrics.FileUploadedBytes += file.UploadedBytes
+		if file.UploadProgress < 100 {
+			metrics.FileUploadsInProgressCount++
+		}
+	}
+
+	// Collect contract stats
+	contracts, err := sc.RenterContractsGet()
+	if err != nil {
+		return metrics, err
+	}
+	metrics.ContractCountActive = len(contracts.ActiveContracts)
+	metrics.ContractCountInactive = len(contracts.InactiveContracts)
+
+	for _, contract := range append(contracts.ActiveContracts, contracts.InactiveContracts...) {
+		metrics.ContractTotalSize += contract.Size
+		metrics.ContractFeeSpending.Add(contract.Fees)
+		metrics.ContractStorageSpending.Add(contract.StorageSpending)
+		metrics.ContractUploadSpending.Add(contract.UploadSpending)
+		metrics.ContractDownloadSpending.Add(contract.DownloadSpending)
+		metrics.ContractRemainingFunds.Add(contract.RenterFunds)
+	}
+
+	// Add up the totals
+	metrics.ContractTotalSpending.
+		Add(metrics.ContractFeeSpending).
+		Add(metrics.ContractStorageSpending).
+		Add(metrics.ContractUploadSpending).
+		Add(metrics.ContractDownloadSpending)
+
+	// Collect wallet stats
+	wallet, err := sc.WalletGet()
+	if err != nil {
+		return metrics, err
+	}
+	metrics.WalletSiacoinBalance = wallet.ConfirmedSiacoinBalance
+	metrics.WalletOutgoingSiacoins = wallet.UnconfirmedOutgoingSiacoins
+	metrics.WalletIncomingSiacoins = wallet.UnconfirmedIncomingSiacoins
+
+	// Collect renter stats
+	renter, err := sc.RenterGet()
+	if err != nil {
+		return metrics, err
+	}
+	metrics.RenterAllowance = renter.Settings.Allowance.Funds
+	metrics.RenterContractFees = renter.FinancialMetrics.ContractFees
+	metrics.RenterTotalAllocated = renter.FinancialMetrics.TotalAllocated
+	metrics.RenterDownloadSpending = renter.FinancialMetrics.DownloadSpending
+	metrics.RenterStorageSpending = renter.FinancialMetrics.StorageSpending
+	metrics.RenterUploadSpending = renter.FinancialMetrics.UploadSpending
+	metrics.RenterUnspent = renter.FinancialMetrics.Unspent
+
+	metrics.APILatency = time.Since(metrics.Timestamp)
+
+	return metrics, nil
+}
+
+// FormatData converts a raw amount of bytes to an easily readable string
+func formatData(v uint64) string {
+	var fmtSize = func(n float64, u string) string {
+		var f string
+		if n > 100 {
+			f = "%5.1f"
+		} else if n > 10 {
+			f = "%5.2f"
+		} else {
+			f = "%5.3f"
+		}
+		return fmt.Sprintf(f+" "+u, n)
+	}
+	if v > 1e12 {
+		return fmtSize(float64(v)/1e12, "TB")
+	} else if v > 1e9 {
+		return fmtSize(float64(v)/1e9, "GB")
+	} else if v > 1e6 {
+		return fmtSize(float64(v)/1e6, "MB")
+	} else if v > 1e3 {
+		return fmtSize(float64(v)/1e3, "kB")
+	}
+	return fmt.Sprintf("%5d  B", v)
+}
---
+++ metrics.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"encoding/csv"
+	"reflect"
+	"strconv"
+	"time"
+
+	"gitlab.com/NebulousLabs/Sia/types"
+)
+
+// Metrics contains all the metrics that will be collected when the metrics
+// collector runs
+type Metrics struct {
+	Timestamp  time.Time     `csv:"timestamp"`
+	APILatency time.Duration `csv:"api_latency"`
+
+	FileCount                  uint64 `csv:"file_count"`
+	FileTotalBytes             uint64 `csv:"file_total_bytes"`
+	FileUploadsInProgressCount uint64 `csv:"file_uploads_in_progress_count"`
+	FileUploadedBytes          uint64 `csv:"file_uploaded_bytes"`
+
+	ContractCountActive      int            `csv:"contract_count_active"`
+	ContractCountInactive    int            `csv:"contract_count_inactive"`
+	ContractTotalSize        uint64         `csv:"contract_total_size"`
+	ContractTotalSpending    types.Currency `csv:"contract_total_spending"`
+	ContractFeeSpending      types.Currency `csv:"contract_fee_spending"`
+	ContractStorageSpending  types.Currency `csv:"contract_storage_spending"`
+	ContractUploadSpending   types.Currency `csv:"contract_upload_spending"`
+	ContractDownloadSpending types.Currency `csv:"contract_download_spending"`
+	ContractRemainingFunds   types.Currency `csv:"contract_remaining_funds"`
+
+	WalletSiacoinBalance   types.Currency `csv:"wallet_siacoin_balance"`
+	WalletOutgoingSiacoins types.Currency `csv:"wallet_outgoing_siacoins"`
+	WalletIncomingSiacoins types.Currency `csv:"wallet_incoming_siacoins"`
+
+	RenterAllowance        types.Currency `csv:"renter_allowance"`
+	RenterContractFees     types.Currency `csv:"renter_contract_fees"`
+	RenterTotalAllocated   types.Currency `csv:"renter_total_allocated"`
+	RenterDownloadSpending types.Currency `csv:"renter_download_spending"`
+	RenterStorageSpending  types.Currency `csv:"renter_storage_spending"`
+	RenterUploadSpending   types.Currency `csv:"renter_upload_spending"`
+	RenterUnspent          types.Currency `csv:"renter_unspent"`
+}
+
+func MetricsHeaders() (headers []string) {
+	t := reflect.TypeOf(Metrics{})
+
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		tag := field.Tag.Get("csv")
+		headers = append(headers, tag)
+	}
+	return headers
+}
+
+// Values marshals all the values to a string and returns them in an array so
+// they can be written to the CSV
+func (m Metrics) Values() (values []string) {
+	return append(values,
+		m.Timestamp.UTC().Format("2006-01-02T15:04:05Z"),
+		m.APILatency.String(),
+		strconv.FormatUint(m.FileCount, 10),
+		strconv.FormatUint(m.FileTotalBytes, 10),
+		strconv.FormatUint(m.FileUploadsInProgressCount, 10),
+		strconv.FormatUint(m.FileUploadedBytes, 10),
+		strconv.Itoa(m.ContractCountActive),
+		strconv.Itoa(m.ContractCountInactive),
+		strconv.FormatUint(m.ContractTotalSize, 10),
+		m.ContractTotalSpending.String(),
+		m.ContractFeeSpending.String(),
+		m.ContractStorageSpending.String(),
+		m.ContractUploadSpending.String(),
+		m.ContractDownloadSpending.String(),
+		m.ContractRemainingFunds.String(),
+		m.WalletSiacoinBalance.String(),
+		m.WalletOutgoingSiacoins.String(),
+		m.WalletIncomingSiacoins.String(),
+		m.RenterAllowance.String(),
+		m.RenterContractFees.String(),
+		m.RenterTotalAllocated.String(),
+		m.RenterDownloadSpending.String(),
+		m.RenterStorageSpending.String(),
+		m.RenterUploadSpending.String(),
+		m.RenterUnspent.String(),
+	)
+}
+
+// WriteCSV adds a row to an existing CSV file with the stored values of Metrics
+func (m Metrics) WriteCSV(f *csv.Writer) error {
+	defer f.Flush()
+	return f.Write(m.Values())
+}
